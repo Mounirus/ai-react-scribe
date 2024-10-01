c07a42154a224d430c536eb19165923d032bab5a
Mounirus <laghamounir@gmail.com>
2024-10-01 12:07:08 +0100

    file upload & audio record

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,10 +1,22 @@
 const FileDisplay = ({ file, audioStream, handleAudioReset }) => {
   return (
-    <main className="flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 md:gap-5 text-center pb-20">
+    <main className="flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 md:gap-5 text-center pb-20 w-72 sm:w-96 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Your <span className="text-blue-400 bold">File</span>
       </h1>
+      <div className="flex flex-col text-left my-4">
+        <h3 className="font-semibold ">Name</h3>
+        <p className="text-base">{file ? file.name : "Custom Audio"}</p>
+      </div>
+      <div className="flex items-center justify-between gap-4">
+        <button onClick={handleAudioReset} className="text-slate-400 hover:text-blue-600 duration-200">Reset</button>
+        <button className="specialBtn px-3 py-2 rounded-lg text-blue-400 flex gap-2 items-center font-medium">
+            <p>Transcribe</p>
+            <i className="fa-solid fa-pen-nib"></i>
+        </button>
+      </div>
     </main>
+
   );
 };
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,69 @@
+import { useEffect, useRef, useState } from "react";
+
 const HomePage = ({ setFile, setAudioStream }) => {
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [duration, setDuration] = useState(0);
+  const [audioChunks, setAudioChunks] = useState([]);
+
+  const mediaRecorder = useRef(null);
+  const mimeType = "audio/webm";
+
+  async function startRecording() {
+    let tempStream;
+
+    console.log("Start recording");
+
+    try {
+      const streamData = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      tempStream = streamData;
+    } catch (err) {
+      console.log(err.message);
+      alert(err.message);
+      return;
+    }
+    setRecordingStatus("recording");
+
+    // Create a new Media recorder instance using the stream
+    const media = new MediaRecorder(tempStream, { type: mimeType });
+    mediaRecorder.current = media;
+
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondtaavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+
+      if (event.data.size > 0) {
+        localAudioChunks.push(event.data);
+      }
+    };
+    setAudioChunks(localAudioChunks);
+  }
+
+  function stopRecording() {
+    console.log("Stop recording");
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      setAudioStream(audioBlob);
+      setAudioChunks([]);
+      //setRecordingStatus("inactive");
+      setDuration(0);
+    };
+  }
+
+  useEffect(() => {
+    if (recordingStatus === "inactive") return;
+    const interval = setInterval(() => {
+      setDuration((curr) => curr + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
   return (
     <main className="flex-1 p-4 flex flex-col justify-center gap-3 sm:gap-4 md:gap-5 text-center pb-20">
       <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
@@ -8,9 +73,24 @@ const HomePage = ({ setFile, setAudioStream }) => {
         Record <span className="text-blue-400">&rarr;</span> Transcribe
         <span className="text-blue-400">&rarr;</span> Translate
       </h3>
-      <button className="flex justify-between gap-4 mx-auto items-center text-base w-72  max-w-full my-4 specialBtn px-4 py-2 rounded-lg">
-        <p className="text-blue-400">Record</p>
-        <i className="fa-solid fa-microphone"></i>
+      <button
+        onClick={
+          recordingStatus === "recording" ? stopRecording : startRecording
+        }
+        className="flex justify-between gap-4 mx-auto items-center text-base w-72  max-w-full my-4 specialBtn px-4 py-2 rounded-lg"
+      >
+        <p className="text-blue-400">
+          {recordingStatus === "inactive" ? "Record" : `Stop recording`}
+        </p>
+        <div className="flex items-center gap-2 ">
+          {duration!==0 && <p className="text-sm ">{duration}s</p>}
+          <i
+            className={
+              "fa-solid fa-microphone duration-200" +
+              (recordingStatus === "recording" ? "text-rose-300" : "")
+            }
+          ></i>
+        </div>
       </button>
       <p className="text-base">
         Or{" "}
@@ -28,7 +108,7 @@ const HomePage = ({ setFile, setAudioStream }) => {
         </label>{" "}
         an mp3 file
       </p>
-      <p className="italic text-slate-500">Free now free forever</p>
+      <p className="italic text-slate-400">Free now free forever</p>
     </main>
   );
 };
